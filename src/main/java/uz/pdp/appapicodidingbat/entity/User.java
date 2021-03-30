251513c2052b06e1d638d9768b32b8d5cfaa6304
Abbos <yulchiyevabbos1@gamil.com>
2021-03-30 16:12:18 +0500

    Edited twice

--- src/main/java/uz/pdp/appapicodidingbat/entity/User.java
+++ src/main/java/uz/pdp/appapicodidingbat/entity/User.java
@@ -27,6 +27,4 @@ public class User {
     private String email;
 
     private String password;
-
-
 }
--- src/main/java/uz/pdp/appapicodidingbat/entity/UserPractice.java
+++ src/main/java/uz/pdp/appapicodidingbat/entity/UserPractice.java
@@ -24,7 +24,7 @@ public class UserPractice {
 
     private Date date;
 
-    @OneToOne
+    @ManyToOne
     private User user;
 
     @OneToOne
